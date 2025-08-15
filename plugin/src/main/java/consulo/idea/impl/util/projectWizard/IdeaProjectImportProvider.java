3b94997f812572ddb7e49b58d85503e358216160
Valery Semenchuk <vistall.valeriy@gmail.com>
2025-08-15 18:46:26 +0400

    migrate to localize value

--- plugin/src/main/java/consulo/idea/impl/util/projectWizard/IdeaProjectImportProvider.java
+++ plugin/src/main/java/consulo/idea/impl/util/projectWizard/IdeaProjectImportProvider.java
@@ -37,6 +37,7 @@ import consulo.language.content.ProductionContentFolderTypeProvider;
 import consulo.language.content.ProductionResourceContentFolderTypeProvider;
 import consulo.language.content.TestContentFolderTypeProvider;
 import consulo.language.content.TestResourceContentFolderTypeProvider;
+import consulo.localize.LocalizeValue;
 import consulo.module.ModifiableModuleModel;
 import consulo.module.Module;
 import consulo.module.content.ModuleRootManager;
@@ -53,9 +54,9 @@ import consulo.ui.ex.wizard.WizardStep;
 import consulo.ui.image.Image;
 import consulo.util.io.FileUtil;
 import consulo.virtualFileSystem.util.VirtualFileUtil;
-
 import jakarta.annotation.Nonnull;
 import jakarta.annotation.Nullable;
+
 import java.io.File;
 import java.util.Collection;
 import java.util.List;
@@ -67,214 +68,175 @@ import java.util.function.Consumer;
  * @since 18:48/14.06.13
  */
 @ExtensionImpl
-public class IdeaProjectImportProvider implements ModuleImportProvider<IdeaImportContext>
-{
-	@Nonnull
-	@Override
-	public String getName()
-	{
-		return "IntelliJ Platform";
-	}
+public class IdeaProjectImportProvider implements ModuleImportProvider<IdeaImportContext> {
+    @Nonnull
+    @Override
+    public LocalizeValue getName() {
+        return LocalizeValue.localizeTODO("IntelliJ");
+    }
 
-	@Nonnull
-	@Override
-	public Image getIcon()
-	{
-		return IdeaImplIconGroup.idea();
-	}
+    @Nonnull
+    @Override
+    public Image getIcon() {
+        return IdeaImplIconGroup.idea();
+    }
 
-	@Override
-	public boolean canImport(@Nonnull File fileOrDirectory)
-	{
-		if(fileOrDirectory.isDirectory())
-		{
-			return new File(fileOrDirectory, IdeaConstants.PROJECT_DIR).exists();
-		}
-		return false;
-	}
+    @Override
+    public boolean canImport(@Nonnull File fileOrDirectory) {
+        if (fileOrDirectory.isDirectory()) {
+            return new File(fileOrDirectory, IdeaConstants.PROJECT_DIR).exists();
+        }
+        return false;
+    }
 
-	@Override
-	public boolean isOnlyForNewImport()
-	{
-		return true;
-	}
+    @Override
+    public boolean isOnlyForNewImport() {
+        return true;
+    }
 
-	@Nonnull
-	@Override
-	public IdeaImportContext createContext(@Nullable Project project)
-	{
-		return new IdeaImportContext(project);
-	}
+    @Nonnull
+    @Override
+    public IdeaImportContext createContext(@Nullable Project project) {
+        return new IdeaImportContext(project);
+    }
 
-	@Override
-	public void buildSteps(@Nonnull Consumer<WizardStep<IdeaImportContext>> consumer, @Nonnull IdeaImportContext context)
-	{
-		Map<String, IdeaModuleTypeConfigurationPanel> configuration = context.getConfiguration();
-		consumer.accept(new UnifiedProjectOrModuleNameStep<>(context));
+    @Override
+    public void buildSteps(@Nonnull Consumer<WizardStep<IdeaImportContext>> consumer, @Nonnull IdeaImportContext context) {
+        Map<String, IdeaModuleTypeConfigurationPanel> configuration = context.getConfiguration();
+        consumer.accept(new UnifiedProjectOrModuleNameStep<>(context));
 
-		if(!configuration.isEmpty())
-		{
-			consumer.accept(new IdeaModuleConfigurationStep(configuration));
-		}
-	}
+        if (!configuration.isEmpty()) {
+            consumer.accept(new IdeaModuleConfigurationStep(configuration));
+        }
+    }
 
-	@RequiredReadAction
-	@Override
-	public void process(@Nonnull IdeaImportContext context, @Nonnull Project project, @Nonnull ModifiableModuleModel newModel, @Nonnull Consumer<Module> consumer)
-	{
-		IdeaProjectModel ideaProjectModel = context.getIdeaProjectModel();
+    @RequiredReadAction
+    @Override
+    public void process(@Nonnull IdeaImportContext context, @Nonnull Project project, @Nonnull ModifiableModuleModel newModel, @Nonnull Consumer<Module> consumer) {
+        IdeaProjectModel ideaProjectModel = context.getIdeaProjectModel();
 
-		if(ideaProjectModel == null)
-		{
-			return;
-		}
+        if (ideaProjectModel == null) {
+            return;
+        }
 
-		List<IdeaModuleModel> ideaModuleModels = ideaProjectModel.getInstance(IdeaModuleTableModel.class).getModules();
+        List<IdeaModuleModel> ideaModuleModels = ideaProjectModel.getInstance(IdeaModuleTableModel.class).getModules();
 
-		Map<String, IdeaModuleTypeConfigurationPanel> map = context.getConfiguration();
+        Map<String, IdeaModuleTypeConfigurationPanel> map = context.getConfiguration();
 
-		for(IdeaModuleModel ideaModuleModel : ideaModuleModels)
-		{
-			File moduleFile = ideaModuleModel.getFile();
+        for (IdeaModuleModel ideaModuleModel : ideaModuleModels) {
+            File moduleFile = ideaModuleModel.getFile();
 
-			String nameWithoutExtension = FileUtil.getNameWithoutExtension(moduleFile);
+            String nameWithoutExtension = FileUtil.getNameWithoutExtension(moduleFile);
 
-			List<IdeaContentEntryModel> contentEntries = ideaModuleModel.getContentEntries();
-			String modulePath = null;
-			if(!contentEntries.isEmpty())
-			{
-				modulePath = VirtualFileUtil.urlToPath(contentEntries.get(0).getUrl());
-			}
+            List<IdeaContentEntryModel> contentEntries = ideaModuleModel.getContentEntries();
+            String modulePath = null;
+            if (!contentEntries.isEmpty()) {
+                modulePath = VirtualFileUtil.urlToPath(contentEntries.get(0).getUrl());
+            }
 
-			Module module = newModel.newModule(nameWithoutExtension, modulePath);
+            Module module = newModel.newModule(nameWithoutExtension, modulePath);
 
-			consumer.accept(module);
+            consumer.accept(module);
 
-			final ModifiableRootModel modifiableModel = ModuleRootManager.getInstance(module).getModifiableModel();
+            final ModifiableRootModel modifiableModel = ModuleRootManager.getInstance(module).getModifiableModel();
 
-			String group = ideaModuleModel.getGroup();
-			if(group != null)
-			{
-				newModel.setModuleGroupPath(module, group.split("/"));
-			}
+            String group = ideaModuleModel.getGroup();
+            if (group != null) {
+                newModel.setModuleGroupPath(module, group.split("/"));
+            }
 
-			for(IdeaContentEntryModel ideaContentEntryModel : contentEntries)
-			{
-				ContentEntry contentEntry = modifiableModel.addContentEntry(ideaContentEntryModel.getUrl());
+            for (IdeaContentEntryModel ideaContentEntryModel : contentEntries) {
+                ContentEntry contentEntry = modifiableModel.addContentEntry(ideaContentEntryModel.getUrl());
 
-				for(IdeaContentFolderModel entry : ideaContentEntryModel.getContentFolders())
-				{
-					ContentFolderTypeProvider provider = ProductionContentFolderTypeProvider.getInstance();
-					if(entry.getBoolProperty("isTestSource"))
-					{
-						provider = TestContentFolderTypeProvider.getInstance();
-					}
+                for (IdeaContentFolderModel entry : ideaContentEntryModel.getContentFolders()) {
+                    ContentFolderTypeProvider provider = ProductionContentFolderTypeProvider.getInstance();
+                    if (entry.getBoolProperty("isTestSource")) {
+                        provider = TestContentFolderTypeProvider.getInstance();
+                    }
 
-					String type = entry.getProperty("type");
-					if("java-resource".equals(type))
-					{
-						provider = ProductionResourceContentFolderTypeProvider.getInstance();
-					}
-					else if("java-test-resource".equals(type))
-					{
-						provider = TestResourceContentFolderTypeProvider.getInstance();
-					}
+                    String type = entry.getProperty("type");
+                    if ("java-resource".equals(type)) {
+                        provider = ProductionResourceContentFolderTypeProvider.getInstance();
+                    }
+                    else if ("java-test-resource".equals(type)) {
+                        provider = TestResourceContentFolderTypeProvider.getInstance();
+                    }
 
-					ContentFolder contentFolder = contentEntry.addFolder(entry.getUrl(), provider);
+                    ContentFolder contentFolder = contentEntry.addFolder(entry.getUrl(), provider);
 
-					if(entry.getBoolProperty("generated"))
-					{
-						contentFolder.setPropertyValue(GeneratedContentFolderPropertyProvider.IS_GENERATED, Boolean.TRUE);
-					}
-				}
-			}
+                    if (entry.getBoolProperty("generated")) {
+                        contentFolder.setPropertyValue(GeneratedContentFolderPropertyProvider.IS_GENERATED, Boolean.TRUE);
+                    }
+                }
+            }
 
-			for(IdeaOrderEntryModel orderEntryModel : ideaModuleModel.getOrderEntries())
-			{
-				OrderEntry orderEntry = null;
-				if(orderEntryModel instanceof ModuleIdeaOrderEntryModel)
-				{
-					orderEntry = modifiableModel.addInvalidModuleEntry(((ModuleIdeaOrderEntryModel) orderEntryModel).getModuleName());
-				}
-				else if(orderEntryModel instanceof ProjectLibraryIdeaOrderEntryModel)
-				{
-					orderEntry = modifiableModel.addInvalidLibrary(((ProjectLibraryIdeaOrderEntryModel) orderEntryModel).getLibraryName(), "project");
-				}
-				else if(orderEntryModel instanceof ModuleLibraryIdeaOrderEntryModel)
-				{
-					IdeaLibraryModel libraryModel = ((ModuleLibraryIdeaOrderEntryModel) orderEntryModel).getLibraryModel();
+            for (IdeaOrderEntryModel orderEntryModel : ideaModuleModel.getOrderEntries()) {
+                OrderEntry orderEntry = null;
+                if (orderEntryModel instanceof ModuleIdeaOrderEntryModel) {
+                    orderEntry = modifiableModel.addInvalidModuleEntry(((ModuleIdeaOrderEntryModel) orderEntryModel).getModuleName());
+                }
+                else if (orderEntryModel instanceof ProjectLibraryIdeaOrderEntryModel) {
+                    orderEntry = modifiableModel.addInvalidLibrary(((ProjectLibraryIdeaOrderEntryModel) orderEntryModel).getLibraryName(), "project");
+                }
+                else if (orderEntryModel instanceof ModuleLibraryIdeaOrderEntryModel) {
+                    IdeaLibraryModel libraryModel = ((ModuleLibraryIdeaOrderEntryModel) orderEntryModel).getLibraryModel();
 
-					Library library = modifiableModel.getModuleLibraryTable().createLibrary(libraryModel.getName());
+                    Library library = modifiableModel.getModuleLibraryTable().createLibrary(libraryModel.getName());
 
-					convertLibrary(library, libraryModel);
+                    convertLibrary(library, libraryModel);
 
-					orderEntry = modifiableModel.findLibraryOrderEntry(library);
-				}
+                    orderEntry = modifiableModel.findLibraryOrderEntry(library);
+                }
 
-				//noinspection ConstantConditions
-				if(orderEntry instanceof ExportableOrderEntry)
-				{
-					((ExportableOrderEntry) orderEntry).setExported(orderEntryModel.isExported());
-				}
-			}
+                //noinspection ConstantConditions
+                if (orderEntry instanceof ExportableOrderEntry) {
+                    ((ExportableOrderEntry) orderEntry).setExported(orderEntryModel.isExported());
+                }
+            }
 
-			String moduleType = ideaModuleModel.getModuleType();
+            String moduleType = ideaModuleModel.getModuleType();
 
-			IdeaModuleTypeConfigurationPanel ideaModuleTypeConfigurationPanel = map.get(moduleType);
-			if(ideaModuleTypeConfigurationPanel != null)
-			{
-				IdeaModuleTypeToModuleExtensionConverter converter = IdeaModuleTypeToModuleExtensionConverter.find(moduleType);
-				assert converter != null;
-				//noinspection unchecked
-				converter.convertTypeToExtension(modifiableModel, ideaModuleModel, ideaModuleTypeConfigurationPanel);
-			}
+            IdeaModuleTypeConfigurationPanel ideaModuleTypeConfigurationPanel = map.get(moduleType);
+            if (ideaModuleTypeConfigurationPanel != null) {
+                IdeaModuleTypeToModuleExtensionConverter converter = IdeaModuleTypeToModuleExtensionConverter.find(moduleType);
+                assert converter != null;
+                //noinspection unchecked
+                converter.convertTypeToExtension(modifiableModel, ideaModuleModel, ideaModuleTypeConfigurationPanel);
+            }
 
-			for(ModuleExtension moduleExtension : modifiableModel.getExtensions())
-			{
-				if(moduleExtension instanceof ModuleExtensionWithSdk)
-				{
-					if(((ModuleExtensionWithSdk) moduleExtension).getInheritableSdk().isNull())
-					{
-						continue;
-					}
-					modifiableModel.addModuleExtensionSdkEntry((ModuleExtensionWithSdk<?>) moduleExtension);
-				}
-			}
+            for (ModuleExtension moduleExtension : modifiableModel.getExtensions()) {
+                if (moduleExtension instanceof ModuleExtensionWithSdk) {
+                    if (((ModuleExtensionWithSdk) moduleExtension).getInheritableSdk().isNull()) {
+                        continue;
+                    }
+                    modifiableModel.addModuleExtensionSdkEntry((ModuleExtensionWithSdk<?>) moduleExtension);
+                }
+            }
 
-			WriteAction.run(modifiableModel::commit);
-		}
+            WriteAction.run(modifiableModel::commit);
+        }
 
-		LibraryTable libraryTable = ProjectLibraryTable.getInstance(project);
+        LibraryTable libraryTable = ProjectLibraryTable.getInstance(project);
 
-		LibraryTable.ModifiableModel libraryTableModifiableModel = libraryTable.getModifiableModel();
+        LibraryTable.ModifiableModel libraryTableModifiableModel = libraryTable.getModifiableModel();
 
-		for(IdeaLibraryModel ideaLibraryModel : ideaProjectModel.getInstance(IdeaProjectLibraryTableModel.class).getLibraries())
-		{
-			Library library = libraryTableModifiableModel.createLibrary(ideaLibraryModel.getName());
+        for (IdeaLibraryModel ideaLibraryModel : ideaProjectModel.getInstance(IdeaProjectLibraryTableModel.class).getLibraries()) {
+            Library library = libraryTableModifiableModel.createLibrary(ideaLibraryModel.getName());
 
-			convertLibrary(library, ideaLibraryModel);
-		}
+            convertLibrary(library, ideaLibraryModel);
+        }
 
-		WriteAction.run(libraryTableModifiableModel::commit);
-	}
+        WriteAction.run(libraryTableModifiableModel::commit);
+    }
 
-	private static void convertLibrary(Library library, IdeaLibraryModel ideaLibraryModel)
-	{
-		Library.ModifiableModel modifiableModel = library.getModifiableModel();
-		for(Map.Entry<IdeaOrderRootType, Collection<String>> entry : ideaLibraryModel.getOrderRoots().entrySet())
-		{
-			for(String url : entry.getValue())
-			{
-				modifiableModel.addRoot(url, entry.getKey().getOrderRootType());
-			}
-		}
+    private static void convertLibrary(Library library, IdeaLibraryModel ideaLibraryModel) {
+        Library.ModifiableModel modifiableModel = library.getModifiableModel();
+        for (Map.Entry<IdeaOrderRootType, Collection<String>> entry : ideaLibraryModel.getOrderRoots().entrySet()) {
+            for (String url : entry.getValue()) {
+                modifiableModel.addRoot(url, entry.getKey().getOrderRootType());
+            }
+        }
 
-		WriteAction.run(modifiableModel::commit);
-	}
-
-	@Override
-	public String getFileSample()
-	{
-		return "<b>IntelliJ Platform</b> project directory (.idea)";
-	}
+        WriteAction.run(modifiableModel::commit);
+    }
 }
